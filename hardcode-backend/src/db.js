6f225dcf96c759b471a08b0a0ab44004419c1efe
YourCasualSenpai <45568983+YourCasualSenpai@users.noreply.github.com>
2026-05-03 02:44:00 -0500

    Instructions for local environment in readme, and added code to set up local environment for testing
    Added code to db.js so that users can test the app through their own local mySQL server.
    Updated the ReadMe.md file.

--- hardcode-backend/src/db.js
+++ hardcode-backend/src/db.js
@@ -1,15 +1,13 @@
+require('dotenv').config();
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-
-    // Password I used for my instance, replace with your own.
-    password: '',
-
-    database: 'queuesys',
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_NAME || 'queuesys',
     waitForConnections: true,
     connectionLimit: 10,
-})
+});
 
 module.exports = pool;
